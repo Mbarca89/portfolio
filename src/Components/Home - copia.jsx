217da137088f61d596f4a1eeb85f95bb7dd78639
Mbarca89 <mauriciobarca1989@gmail.com>
2023-07-25 15:42:58 +0200

    projects gallery

--- src/Components/Home - copia.jsx
+++ src/Components/Home - copia.jsx
@@ -5,7 +5,6 @@ import icons from '../utils/icons'
 import poke from '../utils/poke'
 import byr from '../utils/byr'
 import rym from '../utils/rym'
-import nature from '../utils/nature'
 import top from '../img/top.png'
 import gmail from '../img/gmail.png'
 import github from '../img/github.png'
@@ -18,7 +17,6 @@ import 'aos/dist/aos.css'
 const Home = () => {
 
     const [isHovered, setIsHovered] = useState(false);
-    const [project, setProject] = useState('byr')
 
     const handleMouseEnter = () => {
         setIsHovered(true);
@@ -37,15 +35,9 @@ const Home = () => {
         }
     };
 
-    useEffect(() => {
-        AOS.init({ duration: 500 })
-    }, [])
-
-    const projectHandler = (event) => {
-        setProject(event.target.name)
-    }
-
-    console.log(project)
+    useEffect(()=> {
+        AOS.init({duration: 500})
+    },[])
 
     return (
         <div className={style.home}>
@@ -79,8 +71,8 @@ const Home = () => {
                         <img className={style.code} src={code} alt="" />
                         {/* <div className={`${style.iconsContainer} ${isHovered ? style.hovered : ''}`} onMouseEnter={handleMouseEnter}
                             onMouseLeave={handleMouseLeave} style={getRotationStyles()}> */}
-                        <div className={`${style.iconsContainer} ${style.hovered}`}>
-                            <img className={style.icons1} src={icons.css} alt="" />
+                            <div className={`${style.iconsContainer} ${style.hovered}`}>
+                            <img className={style.icons1} src={icons.css} alt=""/>
                             <img className={style.icons2} src={icons.javaScript} alt="" />
                             <img className={style.icons3} src={icons.react} alt="" />
                             <img className={style.icons4} src={icons.redux} alt="" />
@@ -94,7 +86,7 @@ const Home = () => {
                 <section className={style.title} id='about'>
                     <h3 className={style.titleH3}>About Me!</h3>
                 </section>
-
+                
                 <div className={style.about} data-aos='fade-up'>
                     <div className={style.aboutContainer}>
                         <h1>Hello there! My name is Mauricio Barca</h1>
@@ -124,7 +116,7 @@ const Home = () => {
                 <div className={style.skills} data-aos='fade-up'>
                     <h1>This is what i've learned so far!</h1>
                     <div className={style.skillsContainer}>
-                        <div className={style.gridItem} data-aos='flip-left'>
+                        <div className={style.gridItem}  data-aos='flip-left'>
                             <div className={style.gridDiv}>
                                 <img src={icons.html} alt="" />
                                 <h4>HTML</h4>
@@ -148,13 +140,13 @@ const Home = () => {
                                 <h4>TYPESCRIPT</h4>
                             </div>
                         </div>
-                        <div className={style.gridItem} data-aos='flip-left'>
+                        <div className={style.gridItem}  data-aos='flip-left'>
                             <div className={style.gridDiv}>
                                 <img src={icons.react} alt="" />
                                 <h4>REACT</h4>
                             </div>
                         </div>
-                        <div className={style.gridItem} data-aos='flip-left' data-aos-delay='100'>
+                        <div className={style.gridItem} data-aos='flip-left'  data-aos-delay='100'>
                             <div className={style.gridDiv}>
                                 <img src={icons.redux} alt="" />
                                 <h4>REDUX</h4>
@@ -190,103 +182,48 @@ const Home = () => {
                     <h3 className={style.titleH3}>Projects</h3>
                 </section>
                 <div className={style.projects}>
-                    <div className={style.projectButtons}>
-                        <button name='byr' onClick={projectHandler}>ByR</button>
-                        <button name='nature' onClick={projectHandler}>NaturEssence</button>
-                        <button name='pokemon' onClick={projectHandler}>Pokemon</button>
-                        <button name='rym' onClick={projectHandler}>Rick and Morty</button>
+                    <div className={style.byr}>
+                        <div className={style.byrText}>
+                            <h2>BYR Real State</h2>
+                            <h4>In Progress</h4>
+                            <p>Features for the user: Search properties, filter, view details, live chat, contact form, send whatsapp messages directly from each property detail.</p>
+                            <p>For the administrator: Admin control panel, upload, modify and delete properties, set featured properties to apperar at home page.</p>
+
+                        </div>
+                        <div className={style.byrGallery}>
+                            <img className={style.byr1} src={byr.byr1} alt="" />
+                            <img className={style.byr2} src={byr.byr2} alt="" />
+                            <img className={style.byr3} src={byr.byr3} alt="" />
+                            <img className={style.byr4} src={byr.byr4} alt="" />
+                            <img className={style.byr5} src={byr.byr5} alt="" />
+                        </div>
                     </div>
-                    <div className={style.sliderFrame}>
-                        <ul style={project === 'byr' ? { marginLeft: 0 } : project === 'nature' ? { marginLeft: '-100%' } : project === 'pokemon' ? { marginLeft: '-200%' } : project === 'rym' ? { marginLeft: '-300%' } : null}>
-                            <li>
-                                <div className={style.slide}>
-                                    <div className={style.slideGallery}>
-                                        <img src={byr.byr1} />
-                                    </div>
-                                    <div className={style.slideInfo}>
-                                        <h2>ByR Real State</h2>
-                                        <p>An individual project made for a friend to gain some practice. Still working on it.
-                                            A site for a real state company that features filters, contact form, live chat, details view with image gallery. Also for the owner, an admin panel that allows them to publish properties with images, and details, edit and delete properties and select featured properties to appear at the home page.</p>
-                                        <a href="https://github.com/Mbarca89/BYR-TS-Client">Repository</a>
-                                        <div className={style.techStack}>
-                                            <img src={icons.typeScript} alt="" />
-                                            <img src={icons.react} alt="" />
-                                            <img src={icons.postgres} alt="" />
-                                            <img src={icons.node} alt="" />
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div className={style.slide}>
-                                    <div className={style.slideGallery}>
-                                        <img src={nature.nature1} />
-                                    </div>
-                                    <div className={style.slideInfo}>
-                                        <h2>NaturEssence</h2>
-                                        <p>Natural products marketplace. The platorm includes several key features such as user login, third-party login integration (Google), user
-                                            registration with roles assigned as either customer or seller, email notufications, account activation, password
-                                            reset functionality, product search with combined filters, a shopping cart system, integration with a third-party
-                                            payment platorm, and the ability for users to leave product reviews.
-                                            Additonally, the platorm provides an
-                                            admin/seller dashboard that enables sellers to post, edit, deactivate, and delete products, while administrators
-                                            have the authority to deactivate or delete both products and user accounts</p>
-                                        <a href="https://github.com/Mbarca89/PF-Henry-front">Front end repository</a>
-                                        <a href="https://github.com/Mbarca89/PF-Henry-back">Back end repository</a>
-                                        <a href="https://github.com/Mbarca89/PF-Henry-dash">Dashboard repository</a>
-                                        <a href="https://pf-henry-front-ten.vercel.app/">Deploy</a>
-                                        <div className={style.techStack}>
-                                            <img src={icons.typeScript} alt="" />
-                                            <img src={icons.react} alt="" />
-                                            <img src={icons.redux} alt="" />
-                                            <img src={icons.mongo} alt="" />
-                                            <img src={icons.node} alt="" />
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div className={style.slide}>
-                                    <div className={style.slideGallery}>
-                                        <img src={poke.poke1} />
-                                    </div>
-                                    <div className={style.slideInfo}>
-                                        <h2>Pokemon</h2>
-                                        <p>Individual project of Henry's fullstack developer bootcamp.
-                                            My first project working on my own as an student. Features user registration and login with validated forms, pagination, combined filters, search and sorting, a section to create your pokemon with image upload, edit or delete your pokemons and a "team" section where you can add your own pokemons. As a challenge on this project, we couldn't use external libraries.</p>
-                                        <a href="https://github.com/Mbarca89/pi-pokemon">Repository</a>
-                                        <a href="https://pokemon-henry.onrender.com/">Deploy</a>
-                                        <div className={style.techStack}>
-                                            <img src={icons.javaScript} alt="" />
-                                            <img src={icons.react} alt="" />
-                                            <img src={icons.redux} alt="" />
-                                            <img src={icons.postgres} alt="" />
-                                            <img src={icons.node} alt="" />
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div className={style.slide}>
-                                    <div className={style.slideGallery}>
-                                        <img src={rym.rym2} />
-                                    </div>
-                                    <div className={style.slideInfo}>
-                                        <h2>Rick and Morty</h2>
-                                        <p>An integration project part of Henry's fullstack bootcamp. My first project ever, featuring user login with validated forms, combined filters and sorting, searchbar, and favorites.
-                                            Fullstack project using React-Redux in the frontend and Nodejs in the backend with a postgreSQL database. </p>
-                                        <a href="https://github.com/Mbarca89/Rick-and-Morty">Repository</a>
-                                        <div className={style.techStack}>
-                                            <img src={icons.javaScript} alt="" />
-                                            <img src={icons.react} alt="" />
-                                            <img src={icons.redux} alt="" />
-                                            <img src={icons.postgres} alt="" />
-                                            <img src={icons.node} alt="" />
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
+                    <div className={style.pokemon}>
+                        <div className={style.pokemonGallery}>
+                            <img className={style.poke1} src={poke.poke1} alt="" />
+                            <img className={style.poke2} src={poke.poke2} alt="" />
+                            <img className={style.poke3} src={poke.poke3} alt="" />
+                            <img className={style.poke4} src={poke.poke4} alt="" />
+                            <img className={style.poke5} src={poke.poke5} alt="" />
+                        </div>
+                        <div className={style.pokemonText}>
+                            <h2>Pokemon</h2>
+                            <h4>Fullstack</h4>
+                            <p>Features: User registration and login with validated forms - pagination - combined filters, search and sorting - pokemon creator - edit or delete user pokemons - add pokemons to user's team.</p>
+                        </div>
+                    </div>
+                    <div className={style.rym}>
+                        <div className={style.rymText}>
+                            <h2>Rick and Morty</h2>
+                            <h4>Fullstack</h4>
+                            <p>Features: User login with validated forms, combined filters and sorting, search characters from API, add or delete favorites.</p>
+                        </div>
+                        <div className={style.rymGallery}>
+                            <img className={style.rym1} src={rym.rym1} alt="" />
+                            <img className={style.rym2} src={rym.rym2} alt="" />
+                            <img className={style.rym3} src={rym.rym3} alt="" />
+                            <img className={style.rym4} src={rym.rym4} alt="" />
+                        </div>
                     </div>
                 </div>
                 <section className={style.title} id='contact'>
@@ -299,10 +236,10 @@ const Home = () => {
                     </div>
                     <div className={style.contactInfo}>
                         <a href='mailto:mauriciobarca1989@gmail.com' >
-                            <img src={gmail} alt="" />
+                            <img src={gmail} alt=""/>
                             <p>mauriciobarca1989@gmail.com</p>
                         </a>
-                        <a href="https://github.com/Mbarca89" target="_blank" rel="noreferrer noopener">
+                        <a href="https://github.com/Mbarca89"  target="_blank" rel="noreferrer noopener">
                             <img src={github} alt="" />
                             <p>/Mbarca89</p>
                         </a>
---
+++ src/utils/nature.js
@@ -0,0 +1,5 @@
+const nature = { 
+    nature1:require('../img/naturessence/1.png'),
+}
+
+export default nature
